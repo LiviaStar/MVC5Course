56e35643fc8b9f2eadda92131a22b1af598eae88
LiviaStar <starya215@gmail.com>
2017-05-06 13:22:41 +0800

    新增Model驗證

--- MVC5Course/Models/Product.cs
+++ MVC5Course/Models/Product.cs
@@ -11,7 +11,8 @@ namespace MVC5Course.Models
 {
     using System;
     using System.Collections.Generic;
-    
+    using System.ComponentModel.DataAnnotations;
+
     public partial class Product
     {
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2214:DoNotCallOverridableMethodsInConstructors")]
@@ -19,8 +20,9 @@ namespace MVC5Course.Models
         {
             this.OrderLine = new HashSet<OrderLine>();
         }
-    
+        
         public int ProductId { get; set; }
+        [Required(ErrorMessage ="請輸入商品名稱")]
         public string ProductName { get; set; }
         public Nullable<decimal> Price { get; set; }
         public Nullable<bool> Active { get; set; }
